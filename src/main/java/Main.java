5c5d40638f4edbf2e46bc0bcb02b4331df123d67
neta-p <netapernat@gmail.com>
2024-09-21 19:56:42 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+package lesson1;
+
+public class Main {
+	public static void main(String[] args) {
+		  BankAccount acct1 = new BankAccount();
+	        System.out.println(acct1);
+
+	    }
+	Rectangle neta = new Rectangle(5.5, 5.5);
+    Rectangle shahar = new Rectangle(9.5, 5.5);
+
+    System.out.println("Drawing the 'neta' rectangle:");
+    neta.draw();
+    System.out.println("Drawing the 'shahar' rectangle:");
+    shahar.draw();
+
+    System.out.println("Drawing the 'neta' rectangle after doubling:");
+   
+
+    double areaNeta = neta.calcArea();
+    double areaShahar = shahar.calcArea();
+   
+
+}
+}
 }
